353f670b20a44063abcea3214f64bc65e26db476
Loyiso <Loyiso@projectcodex.co>
2017-10-18 12:21:37 +0200

    added new features

--- public/shoe.js
+++ public/shoe.js
@@ -1,197 +1,219 @@
 $(function() {
 
-            var display = document.getElementById("display");
+    var display = document.getElementById("display");
 
-            var dropDownDisplay = document.querySelector("#drpdwn");
-            var dis = document.querySelector("#dis");
-            var brandropDowns = document.getElementById("newBrandOption").innerHTML;
-            var brandDrops = Handlebars.compile(brandropDowns);
-            var colordropDowns = document.getElementById("newColorOption").innerHTML;
-            var colorDrops = Handlebars.compile(colordropDowns);
+    var dropDownDisplay = document.querySelector("#drpdwn");
+    var dis = document.querySelector("#dis");
+    var brandropDowns = document.getElementById("newBrandOption").innerHTML;
+    var brandDrops = Handlebars.compile(brandropDowns);
+    var colordropDowns = document.getElementById("newColorOption").innerHTML;
+    var colorDrops = Handlebars.compile(colordropDowns);
 
-            var table = document.getElementById("output").innerHTML;
-            var theTemplate = Handlebars.compile(table);
+    var table = document.getElementById("output").innerHTML;
+    var theTemplate = Handlebars.compile(table);
 
-            $.ajax({
-                    type: "GET",
-                    url: 'http://localhost:3007/api/shoe',
+    $.ajax({
+        type: "GET",
+        url: 'http://localhost:3007/api/shoe',
 
-                    success: function(data) {
-                        var uniqueBrand = [];
-                        var brandMap = {};
-                        for (var i = 0; i < data.length; i++) {
-                            var assets = data[i]
-                            if (brandMap[assets.brand] === undefined) {
-                                brandMap[assets.brand] = assets.brand;
-                                uniqueBrand.push(assets.brand)
-                            }
-                        }
+        success: function(data) {
+            var uniqueBrand = [];
+            var brandMap = {};
+            for (var i = 0; i < data.length; i++) {
+                var assets = data[i]
+                if (brandMap[assets.brand] === undefined) {
+                    brandMap[assets.brand] = assets.brand;
+                    uniqueBrand.push(assets.brand)
+                }
+            }
 
-                        var uniqueColor = [];
-                        var colorMap = {};
-                        for (var a = 0; a < data.length; a++) {
-                            var colorate = data[a]
-                            if (colorMap[colorate.color] === undefined) {
-                                colorMap[colorate.color] = colorate.color;
-                                uniqueColor.push(colorate.color)
+            var uniqueColor = [];
+            var colorMap = {};
+            for (var a = 0; a < data.length; a++) {
+                var colorate = data[a]
+                if (colorMap[colorate.color] === undefined) {
+                    colorMap[colorate.color] = colorate.color;
+                    uniqueColor.push(colorate.color)
 
-                            }
-                          }
-                            display.innerHTML = theTemplate({
-                                stock: data
-                            })
-
-                            dis.innerHTML = brandDrops({
-                                stock: uniqueBrand.sort()
-                            })
-
-                            dropDownDisplay.innerHTML = colorDrops({
-                                stock: uniqueColor.sort()
-                            })
-                        },
-                        error: function(error) {}
-                    })
-
-
-                $('#showAll').on("click", function() {
-                    $.ajax({
-                        type: "GET",
-                        url: 'http://localhost:3007/api/shoe',
-
-                        success: function(data) {
-
-                          var uniqueBrand = [];
-                          var brandMap = {};
-                          for (var i = 0; i < data.length; i++) {
-                              var assets = data[i]
-                              if (brandMap[assets.brand] === undefined) {
-                                  brandMap[assets.brand] = assets.brand;
-                                  uniqueBrand.push(assets.brand)
-                              }
-                          }
-
-                          var uniqueColor = [];
-                          var colorMap = {};
-                          for (var a = 0; a < data.length; a++) {
-                              var colorate = data[a]
-                              if (colorMap[colorate.color] === undefined) {
-                                  colorMap[colorate.color] = colorate.color;
-                                  uniqueColor.push(colorate.color)
-
-                              }
-                            }
-
-                            display.innerHTML = theTemplate({
-                                stock: data
-                            })
-
-                            dis.innerHTML = brandDrops({
-                                stock: uniqueBrand
-                            })
-
-                            dropDownDisplay.innerHTML = colorDrops({
-                                stock: uniqueColor
-                            })
-                        },
-                        error: function(error) {}
-                    })
-
-                })
-
-                $('#show').on('click', function() {
-                    var brand = document.getElementById('brand').value;
-                    var color = document.getElementById('color').value;
-                    var size = document.getElementById('size').value;
-                    var stockAvai = document.getElementById('InStock').value;
-                    var price = document.getElementById('Price').value;
-
-                    var display = document.getElementById("display");
-
-                    var shoes = {
-                        brand: brand,
-                        color: color,
-                        size: size,
-                        in_stock: stockAvai,
-                        price: price
-                    }
-
-                    $.ajax({
-                        type: "POST",
-                        url: 'http://localhost:3007/api/shoe',
-                        dataType: "json",
-                        data: shoes,
-                        success: function(data) {
-                            window.location.reload()
-                            // display.innerHTML = theTemplate({
-                            //     stock: data.shoes
-                            // })
-                        },
-                        error: function(error) {
-
-                        }
-                    })
-
-                })
-
-                $('#dis').on('click', function(e) {
-
-                    var brandOption = e.target.value;
-
-                    $.ajax({
-                        type: "GET",
-                        url: 'http://localhost:3007/api/shoe/brand/' + brandOption,
-                        success: function(data) {
-                            console.log(data);
-                            display.innerHTML = theTemplate({
-                                stock: data
-                            })
-                        },
-                        error: function(error) {
-
-                        }
-                    })
-                })
-
-                $('#drpdwn').on('click', function(e) {
-
-                    var colorOption = e.target.value;
-
-                    $.ajax({
-                        type: "GET",
-                        url: 'http://localhost:3007/api/shoe/color/' + colorOption,
-                        success: function(data) {
-                            display.innerHTML = theTemplate({
-                                stock: data
-                            })
-                        },
-                        error: function(error) {
-
-                        }
-                    })
-                })
-
-
-                $('#display').on('click', function(e) {
-
-                    var id = e.target.value;
-
-                    $.ajax({
-                        type: "POST",
-                        url: 'http://localhost:3007/api/shoe/sold/' + id,
-                        dataType: "json",
-
-                        success: function(data) {
-
-                            window.location.reload()
-                            // display.innerHTML = theTemplate({
-                            //     stock: data
-                            // })
-                        },
-                        error: function(error) {
-
-                        }
-                    })
-                })
-
-
+                }
+            }
+            display.innerHTML = theTemplate({
+                stock: data
             })
+
+            dis.innerHTML = brandDrops({
+                stock: uniqueBrand.sort()
+            })
+
+            dropDownDisplay.innerHTML = colorDrops({
+                stock: uniqueColor.sort()
+            })
+        },
+        error: function(error) {}
+    })
+
+
+    $('#showAll').on("click", function() {
+        $.ajax({
+            type: "GET",
+            url: 'http://localhost:3007/api/shoe',
+
+            success: function(data) {
+
+                var uniqueBrand = [];
+                var brandMap = {};
+                for (var i = 0; i < data.length; i++) {
+                    var assets = data[i]
+                    if (brandMap[assets.brand] === undefined) {
+                        brandMap[assets.brand] = assets.brand;
+                        uniqueBrand.push(assets.brand)
+                    }
+                }
+
+                var uniqueColor = [];
+                var colorMap = {};
+                for (var a = 0; a < data.length; a++) {
+                    var colorate = data[a]
+                    if (colorMap[colorate.color] === undefined) {
+                        colorMap[colorate.color] = colorate.color;
+                        uniqueColor.push(colorate.color)
+
+                    }
+                }
+
+                display.innerHTML = theTemplate({
+                    stock: data
+                })
+
+                dis.innerHTML = brandDrops({
+                    stock: uniqueBrand
+                })
+
+                dropDownDisplay.innerHTML = colorDrops({
+                    stock: uniqueColor
+                })
+            },
+            error: function(error) {}
+        })
+
+    })
+
+    $('#show').on('click', function() {
+        var brand = document.getElementById('brand').value;
+        var color = document.getElementById('color').value;
+        var size = document.getElementById('size').value;
+        var stockAvai = document.getElementById('InStock').value;
+        var price = document.getElementById('Price').value;
+
+        var display = document.getElementById("display");
+
+        var shoes = {
+            brand: brand,
+            color: color,
+            size: size,
+            in_stock: stockAvai,
+            price: price
+        }
+
+        $.ajax({
+            type: "POST",
+            url: 'http://localhost:3007/api/shoe',
+            dataType: "json",
+            data: shoes,
+            success: function(data) {
+                window.location.reload()
+                // display.innerHTML = theTemplate({
+                //     stock: data.shoes
+                // })
+            },
+            error: function(error) {
+
+            }
+        })
+
+    })
+
+    $('#dis').on('click', function(e) {
+
+        var brandOption = e.target.value;
+
+        $.ajax({
+            type: "GET",
+            url: 'http://localhost:3007/api/shoe/brand/' + brandOption,
+            success: function(data) {
+                console.log(data);
+                display.innerHTML = theTemplate({
+                    stock: data
+                })
+            },
+            error: function(error) {
+
+            }
+        })
+    })
+
+    $('#drpdwn').on('click', function(e) {
+
+        var colorOption = e.target.value;
+
+        $.ajax({
+            type: "GET",
+            url: 'http://localhost:3007/api/shoe/color/' + colorOption,
+            success: function(data) {
+                display.innerHTML = theTemplate({
+                    stock: data
+                })
+            },
+            error: function(error) {
+
+            }
+        })
+    })
+
+
+    $('#display').on('click', function(e) {
+
+        var id = e.target.value;
+
+        $.ajax({
+            type: "POST",
+            url: 'http://localhost:3007/api/shoe/sold/' + id,
+            dataType: "json",
+
+            success: function(data) {
+
+                window.location.reload()
+                // display.innerHTML = theTemplate({
+                //     stock: data
+                // })
+            },
+            error: function(error) {
+
+            }
+        })
+    })
+
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+
+
+})
